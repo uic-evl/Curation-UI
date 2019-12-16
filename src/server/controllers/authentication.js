28f9748b0ce24acc4005628c974c4d413c54fa66
Juan Trelles <juantrelles@gmail.com>
2019-12-16 11:24:18 -0600

    fix numberTasks is null issue, typo in userId in controller and missing userId in reducer

--- src/server/controllers/authentication.js
+++ src/server/controllers/authentication.js
@@ -37,7 +37,7 @@ exports.signin = function(req, res, next) {
   // If user is authenticated, give them the token
   console.log(req.user);
   res.send({
-    user_id: req.user._id,
+    userId: req.user._id,
     username: req.user.username,
     roles: req.user.roles,
     access: req.user.access,
